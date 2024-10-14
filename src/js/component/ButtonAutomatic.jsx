a8c1808e39fc81a4637ae8684f1fbf0c131fe547
Yarom Vargas <yaromvp@gmail.com>
2024-10-14 22:39:03 +0000

    Finalizado con ayuda de José - yaromvp

--- src/js/component/ButtonAutomatic.jsx
+++ src/js/component/ButtonAutomatic.jsx
@@ -24,52 +24,43 @@ export const ButtonAutomatic = (props) => {
 
     // Function to start the timer
     const clickAutomatic = () => {
-        // Use setInterval to update the timer every 1000 milliseconds (1 second)
-        setTimeInterval(setInterval(() => {
-            // Update the timer by incrementing the previous value by 1
-            setTimer((prev) => prev + 1);
-        }, 1000));
-
+        if (timeInterval != null) {
+            clearInterval(timeInterval);
+            setTimeInterval(null);
+        }
+        else {
+            setTimeInterval(setInterval(() => {
+                setTimer((prev) => prev + 1);
+            }, 1000));
+        }
         props.setBtnAuto(
             (prev) => !prev
         )
     }
 
-    let btnColor = "primary"
-    let conditionalColor = ""
-
-    if (props.btnAuto) {
-        if (timer > 0 && timer <= 2) {
-            props.setColor("red")
-            conditionalColor = "red"
-            btnColor = "danger"
-            //console.log("Luz roja 🔴 ", timer, " auto: ", btnColor)
-        } else if (timer > 2 && timer <= 4) {
-            props.setColor("yellow")
-            conditionalColor = "yellow"
-            btnColor = "warning"
-            //console.log("Luz amarilla 🟡 ", timer, " auto: ", btnColor)
-        } else if (timer > 4 && timer <= 6) {
-            props.setColor("green")
-            conditionalColor = "green"
-            btnColor = "success"
-            //console.log("Luz verde 🟢 ", timer, " auto: ", btnColor)
-        } else if (timer > 6) {
-            props.setColor("")
-            conditionalColor = ""
-            btnColor = "primary"
-            setTimer(1);
-            //clearInterval(timeInterval);
+    useEffect(() => {
+        if (props.btnAuto) {
+            if (timer > 0 && timer <= 2) {
+                props.setColor("red")
+            } else if (timer > 2 && timer <= 4) {
+                props.setColor("yellow")
+            } else if (timer > 4 && timer <= 6) {
+                props.setColor("green")
+            } else if (timer > 6) {
+                props.setColor("")
+                setTimer(1);
+                //clearInterval(timeInterval);
+            }
         }
-    }
-    else {
-        clearInterval(timeInterval);
-        //conditionalColor == "" ? setTimer(0) : null
-    }
+        else {
+            clearInterval(timeInterval);
+        }
+    }, [timer])
 
     return (
         <div className="d-flex justify-content-center mt-3">
-            <button className={`btn btn-${props.color == conditionalColor ? btnColor : "primary"}`} onClick={clickAutomatic}>Automatic</button>
+            <button className={`btn btn-${props.color == "red" ? "danger" : props.color == "yellow" ? "warning" : props.color == "green" ? "success" : "primary"
+                }`} onClick={clickAutomatic}>Automatic</button>
         </div >
     );
 };
